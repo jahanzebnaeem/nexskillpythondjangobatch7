71ee44bb955f7c248c8de53826db6395d1453ddf
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-03-12 18:45:36 +0500

    Lecture 1
    Data types in python.

---
+++ python/datatypes.py
@@ -0,0 +1,13 @@
+fName = "Jahanzeb's"
+lName = 'Naeem'
+fullName = 'Jahanzeb Naeem'
+# print(fName + ' ' + lName)
+# print(fName[0:5]+fName[5:])
+# print('before changing')
+# print(fName.split('a'))
+# print('after changing')
+# fName = 'Ali'
+# print(fName)
+print(len(fName))
+# a = True
+# print(a)
