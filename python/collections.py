cc0c83de4371ff7f6e01909742b069ea49b280b4
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-03-14 06:12:54 +0500

    Lecture 2
    Python Lists, Tuples, Sets & Dictionaries.

---
+++ python/collections.py
@@ -0,0 +1,40 @@
+## LIST
+
+# myList = [1, 2, 3, 4, 5, 6]
+# print(myList[4:])
+# myMatrix = [[1,2], [3,4], [5,6]]
+# myMatrix.append([7,6])
+# print(myMatrix)
+
+# myList1 = [1, 2, 3 , 4, 5]
+# myList2 = [6, 7, 8 , 9, 8, 8, 10]
+# # myList3 = myList1 + myList2
+# # print(myList3)
+# myList2.pop(3)
+# print(myList2)
+
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(letters.index('e'))
+# letters.pop(4)
+# print(letters)
+# myList2.remove(8)
+# print(myList2)
+
+
+## TUPLE
+# myTuple = (1, 2, 3, 5, 6, 6, 7)
+# print(myTuple[4:])
+
+## SETS
+# a = set('abcdefghi')
+# b = set('alacazam')
+# print(a^b)
+
+## DICTIONARIES
+# myDict = {'name': 'Ali', 'age': 20, 'height': 5}
+# myDict['weitht'] = 65
+# list(myDict)
+# print(list(myDict.values()))
+# print(sorted(myDict))
+myDict2 = dict(name='Ali', age=20)
+print(myDict2)
