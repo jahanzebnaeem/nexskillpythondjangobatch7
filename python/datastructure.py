a79ca5b2b8c62d9c0880c44efd92232250e6fe54
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-03-20 16:26:43 +0500

    Lecture 4
    Data structures, List comprehension.

---
+++ python/datastructure.py
@@ -0,0 +1,37 @@
+# import collections
+#
+# deq = collections.deque([1,2,3,4])
+#
+# deq.append(5)
+# print(deq)
+# deq.popleft()
+# print(deq)
+
+# stack = [2,3]
+# stack.append(4)
+# stack.append(5)
+# print(stack)
+# # Stack
+# # stack.pop()
+# Queue
+# stack.popleft()
+# print(stack)
+
+# List Comprehension
+# Step 1
+# squares = []
+# for x in range(10):
+#     squares.append(x**2)
+# Step 2
+# squares = list(map(lambda x: x**2, range(10)))
+# Step 3
+# squares = [x**2 for x in range(10)]
+# print(squares)
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+print(matrix)
