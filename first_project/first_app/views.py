8d187ad61e142d0c3a8d28bd646543e410ef5800
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-04-09 23:19:18 +0500

    Lecture 9
    Templating in django

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def index(request):
     # return HttpResponse("Hello World")
     my_dict = {'insert_me':"Hello I am from views.py"}
-    return render(request, 'index.html', context=my_dict)
+    return render(request, 'first_app/index.html', context=my_dict)
